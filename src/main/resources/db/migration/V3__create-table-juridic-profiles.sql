d76f88329555335dfc931bd787e75b11e5cda5d6
Lucas Marchesoni <lucasmarchesoni620@gmail.com>
2024-04-11 09:55:39 -0300

    feat: add juridic profile migration

---
+++ src/main/resources/db/migration/V3__create-table-juridic-profiles.sql
@@ -0,0 +1,21 @@
+CREATE TABLE juridic_profiles (
+    id VARCHAR(255) PRIMARY KEY,
+    user_id VARCHAR(255) NOT NULL,
+    legal_name VARCHAR(255) NOT NULL,
+    cnpj VARCHAR(18) UNIQUE NOT NULL,
+    state_registration VARCHAR(20),
+    trade_name VARCHAR(255),
+    activity_sector VARCHAR(100),
+    commercial_address VARCHAR(255),
+    commercial_phone VARCHAR(20),
+    website VARCHAR(255),
+    commercial_email VARCHAR(255),
+    company_description VARCHAR(255),
+    company_logo VARCHAR(255),
+    legal_representative_contact_name VARCHAR(255),
+    legal_representative_contact_email VARCHAR(255),
+    legal_representative_contact_phone VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT juridic_profiles_user_id_fkey FOREIGN KEY (user_id) REFERENCES juridic_profiles(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
